ac6684778e6b2b103c85aa1649ea5b6073d5a2a9
daisp <337596771@qq.com>
2020-04-23 15:36:24 +0800

    左程云初级教程： 1、归并排序 2、数组小和 3、逆序对

---
+++ java/pers/daisp/util/ArrayUtil.java
@@ -0,0 +1,26 @@
+package pers.daisp.util;
+
+public class ArrayUtil {
+    public static int[] createRandomArray1(int length){
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = i;
+        }
+        for (int i = 0; i < length; i++) {
+            int random = (int) (length * Math.random());
+            int temp = result[i];
+            result[i] = result[random];
+            result[random] = temp;
+        }
+        return result;
+    }
+
+    public static int[] createRandomArray2(int length){
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            int random = (int) (length * Math.random());
+            result[i] = random;
+        }
+        return result;
+    }
+}
---
+++ java/pers/daisp/zuo/MergeSort.java
@@ -0,0 +1,45 @@
+package pers.daisp.zuo;
+
+import pers.daisp.util.ArrayUtil;
+
+import java.util.Arrays;
+
+/**
+ * 归并排序
+ */
+public class MergeSort {
+    static int help[];
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left == right) return ;
+        int mid = left + ((right - left) >> 1);
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        mergeArray(arr, left, mid, right);
+    }
+
+    private static void mergeArray(int[] arr, int left, int mid, int right) {
+        int i = left, index = left;
+        int j = mid + 1;
+        while (i <= mid && j <= right) {
+            help[index ++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+        }
+        while (i <= mid) {
+            help[index++] = arr[i++];
+        }
+        while (j <= right) {
+            help[index++] = arr[j++];
+        }
+        for (index = left; index <= right; index ++) {
+            arr[index] = help[index];
+        }
+    }
+
+    public static void main(String[] args) {
+        help = new int[10];
+        for (int i = 0; i < 100; i++) {
+            int array[] = ArrayUtil.createRandomArray2(10);
+            mergeSort(array, 0, 9);
+            System.err.println(Arrays.toString(array));
+        }
+    }
+}
